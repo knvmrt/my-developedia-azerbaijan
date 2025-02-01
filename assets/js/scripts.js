62bbcf5fd5797cb1e0bf40918b8b1e265d07e476
Murad KnV <knvmrt@gmail.com>
2025-02-01 16:22:40 +0400

    Footer problem control.

--- assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const yearElement = document.getElementById("current-year");
-    if (yearElement) {
-        const currentYear = new Date().getFullYear();
-        yearElement.textContent = currentYear;
-    }
-});
